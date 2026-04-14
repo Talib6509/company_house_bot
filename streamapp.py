7f6c708191ba7a6594f4f5e34ad40f5935afcc9c
Talib ul haq <43356060+Talib6509@users.noreply.github.com>
2026-04-14 10:11:50 +0530

    Update streamapp.py

--- streamapp.py
+++ streamapp.py
@@ -12,7 +12,7 @@ col1, col2 = st.columns([1, 6])
 with col1:
     st.image("logo.png", width=80)
 with col2:
-    st.title("Company House Chat")
+    st.title("Companies House Chat")
 
 # -------------------------------
 # SESSION STATE
