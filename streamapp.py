b04771287f860148949802c0bb8e4068a5eb759a
Talib ul haq <43356060+Talib6509@users.noreply.github.com>
2026-04-14 13:13:33 +0530

    Fix typo in title from 'Companies' to 'Company'

--- streamapp.py
+++ streamapp.py
@@ -12,7 +12,8 @@ col1, col2 = st.columns([1, 6])
 with col1:
     st.image("logo.png", width=80)
 with col2:
-    st.title("Companies House Chat")
+    st.title("Company House Chat")
+
 
 # -------------------------------
 # SESSION STATE
